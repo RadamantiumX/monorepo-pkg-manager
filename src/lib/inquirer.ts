3227fdc3f868314814014dac79c683f507617a2f
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-05-14 23:16:12 -0300

    feat: inquirer function created

--- src/lib/inquirer.ts
+++ src/lib/inquirer.ts
@@ -1,4 +1,35 @@
+import { select, confirm, input } from "@inquirer/prompts";
+import options from "./options-choices";
+import type { PromptValues } from "../types";
+import colors from "ansi-colors";
+
 /**
- * Here put inquirer prompts methods to use: select, confirm, input, etc
- * Use any conditional to manage them
+ * Takes the console prompts and return the answers
+ * @returns
  */
+export async function promptHandler(): Promise<PromptValues | void> {
+  try {
+    const selectFolder = await select({
+      message: "",
+      choices: [],
+    });
+    const selectPkgManager = await select(options.pkg_manager);
+    const inputPkg = await input(options.pkg);
+    const depsConfirm = await confirm(options.deps);
+    const finishConfirm = await confirm(options.finish);
+    if (!finishConfirm) {
+      console.log(colors.bgMagenta("Installation cancelled..."));
+      return;
+    }
+    return {
+      selectFolder,
+      selectPkgManager,
+      inputPkg,
+      depsConfirm,
+      finishConfirm,
+    };
+  } catch (error) {
+    console.log(`Something went wrong: ${error}`);
+    return;
+  }
+}
