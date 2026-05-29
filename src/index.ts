c0cf1813083be3d01e6d332fbffb0d7780949e35
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-05-29 18:01:13 -0300

    feat: index config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { getWorkSpaceFolders } from "./streams/sub-process";
 import { colorConsole } from "./console/colors";
+import { parsedStreamData } from "./common/parsed";
+import { promptHandler } from "./lib/inquirer";
+import { commandAssembly } from "./common/command";
+import { onFinishPrompts } from "./streams/sub-process";
+import type { PromptValues } from "./types/index";
 
 const turbo = getWorkSpaceFolders();
 
@@ -8,7 +13,32 @@ turbo?.on("data", wsMainInterface);
 
 export async function wsMainInterface(data: any) {
   try {
-    console.log(data);
+    parsedStreamData(data);
+    const {
+      selectFolder,
+      selectPkgManager,
+      inputPkg,
+      depsConfirm,
+      finishConfirm,
+    } = (await promptHandler()) as PromptValues;
+    if (!finishConfirm) {
+      colorConsole("Exit from the assistant", "exit");
+      return;
+    }
+    const cmdStatment = commandAssembly(
+      selectFolder,
+      selectPkgManager,
+      inputPkg,
+      depsConfirm,
+    );
+
+    onFinishPrompts(
+      cmdStatment?.first as string,
+      cmdStatment.second as string,
+      cmdStatment.third as string,
+    );
+
+    return;
   } catch (error) {
     colorConsole(`${error}`, "error");
   }
