3a5febec5f1a86d03ffc2963be573b3a3551ce91
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-05-30 01:06:32 -0300

    feat: adding comments

--- src/common/command.ts
+++ src/common/command.ts
@@ -1,17 +1,24 @@
+import type { Statement } from "../types";
 /**
- * All Statements structures Guidance:
+ * Statements structures Guidance:
  *  --> npm install <package-name> --workspace=<workspace-name> <if is dev-dep "-D" or "--save-dev">
  *  --> pnpm add <if is dev-dep "-D"> <package-name> <flag: filter workspace>
  *  --> yarn workspace <workspace-name> add <if is dev-dep "-D"> <package-name>
- *
- *
+ */
+/**
+ * Takes the package manager selected and create the object with the complete install command
+ * @param {string} selectFolder Workspace folder selected.
+ * @param {string} selectPkgManager Package manager selected, can be npm, pnpm or yarn
+ * @param {string} inputPkg Current dependency to install
+ * @param {boolean} depsConfirm If the dependency it's only for development
+ * @returns {Statement} All elements in order to be used into child process
  */
 export function commandAssembly(
   selectFolder: string,
   selectPkgManager: string,
   inputPkg: string,
   depsConfirm: boolean,
-) {
+): Statement {
   const command = {
     ...(selectPkgManager === "npm" && {
       first: `${selectPkgManager} i`,
